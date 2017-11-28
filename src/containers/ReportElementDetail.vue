d3d1d0a4b76ebaf149a517bc0fda81f88d5dcadc
PROJE\gokay.ortasoz <asmimz4Q5>
2017-11-28 18:24:14 +0300

    report element detail top actions fixes

--- src/containers/ReportElementDetail.vue
+++ src/containers/ReportElementDetail.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
+  <div class="row">
+    <div class="nav-tabs-navigation">
+      <div class="nav-tabs-wrapper">
+        <ul class="nav nav-tabs pull-left ">
+            <li>
+              <span class="button-holder title title_center">Back to Dashboard</span></a>
+            </li>
+          </ul>
+
+            <ul  class="top-related-actions pull-right" style="position:absolute; top:0; right:0">
+              <li> <i  class="icon-query"></i> Query</li>
+              <li> <i  class="icon-refresh"></i> Refresh</li>
+              <li> <i  class="icon-export"></i> Export</li>
+              <li> <i  class="icon-export"></i> Save</li>
+            </ul>
+        </div>
+      </div>
+
     <table class="table" v-if="queryResult.schema">
       <thead>
         <th v-for="field in queryResult.schema.fields">{{field.name}}</th>
@@ -56,5 +73,18 @@
   }
 </script>
 <style>
+span.button-holder, .fixed-top-bar ul li span {
+    font-size: 13px;
+    font-weight: 500;
+    text-align: left;
+        list-style: none;
+    float: left;
+    padding: 11px 18px 13px 18px;
+    cursor: pointer;
+}
 
+.top-related-actions li  { list-style: none;
+    float: left;
+    padding: 11px 18px 13px 18px;
+    cursor: pointer;}
 </style>
