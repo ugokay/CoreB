337e934cda70654155a790079db53054f1629513
PROJE\gokay.ortasoz <asmimz4Q5>
2017-12-08 17:06:03 +0300

    mustache progress baar

--- src/components/ReportElement.vue
+++ src/components/ReportElement.vue
@@ -27,8 +27,12 @@
       </div>
     </div>
     <div v-if="loading">
-      <div v-if="progress > 0">{{progress}}</div>
-      <div v-else="">Loading...</div>
+      <div class="progress-bar" style="width: {{progress}} %" v-if="progress > 0">{{progress}}</div>
+      <div class="loader" v-else="">Loading...</div>
+
+
+
+
     </div>
     <div v-if="queryResult.schema">
       <div v-if="element.chartType === 6">
@@ -171,4 +175,66 @@
     display: block!important;
     z-index: 9999;
   }
+
+
+
+
+  .loader,
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #000040;
+  font-size: 10px;
+  margin: 80px auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: '';
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+@-webkit-keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+@keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+
 </style>
