844d204e289fccaf6add0a8b45db6fcf398a9a45
Görkem <gorkem.gok@proj-e.com>
2017-12-21 14:26:23 +0300

    Seperated dev,test and prod configs (API_URL)

--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"http://localhost:8081/rest-api/v1/"'
+  NODE_ENV: '"test"',
+  API_URL: '"http://52.164.225.245:8081/rest-api/v1/"'
 })
