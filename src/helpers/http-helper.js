dad4ba38865e2fad7a8df1de5bde2591e8594e11
Görkem <gorkem.gok@proj-e.com>
2017-12-22 11:49:01 +0300

    Changed serve host to 0.0.0.0 for mobile debuging

--- src/helpers/http-helper.js
+++ src/helpers/http-helper.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  // baseURL: '/rest-api/v1/'
-  // baseURL: 'http://52.164.225.245:8081/rest-api/v1/'
-  // baseURL: 'http://localhost:8081/rest-api/v1/'
   baseURL: process.env.API_URL
 })
